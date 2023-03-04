078521658a4d26f5078aafcf50ac7474bc7bcfc8
Yuanmingyangymy <106372033+Yuanmingyangymy@users.noreply.github.com>
2023-03-04 21:20:16 +0800

    侧边栏基本完成

---
+++ src/views/Main.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <el-container>
+      <el-aside width="200px">
+        <CommonAside/>
+      </el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>
+          <!-- <h1>main</h1> -->
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import CommonAside from '../components/CommonAside.vue'
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  components: {
+    CommonAside
+  }
+}
+</script>
+
+<style>
+
+</style>
