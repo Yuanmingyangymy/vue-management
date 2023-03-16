1d95642e5eb54f04cb942279cd0de4fb6be60205
Yuanmingyangymy <106372033+Yuanmingyangymy@users.noreply.github.com>
2023-03-16 19:26:17 +0800

    首页左侧
    首页左侧的userinfo和表格

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,138 @@
 <template>
-  <h1>Home</h1>
+  <el-row>
+    <el-col :span="8">
+      <el-card class="box-card">
+        <div class="user">
+          <img src="../assets/images/user.png" alt="用户头像">
+          <div class="userinfo">
+            <p class="name">Admin</p>
+            <p class="access">超级管理员</p>
+          </div>
+        </div>
+        <div class="login-info">
+          <p>上次登录时间：<span>2023-3-15</span></p>
+          <p>上次登录地点：<span>西安</span></p>
+        </div>
+      </el-card>
+      <el-card style="margin-top: 20px; height: 460px;">
+        <el-table
+        :data="tableData"
+        style="width: 100%">
+          <!-- <el-table-column
+            prop="name"
+            label="名称"
+          >
+          </el-table-column>
+          <el-table-column
+            prop="todayBuy"
+            label="今日购买"
+          >
+          </el-table-column>
+          <el-table-column
+            prop="monthBuy"
+            label="本月购买"
+          >
+          </el-table-column>
+          <el-table-column
+            prop="totalBuy"
+            label="总共购买"
+          >
+          </el-table-column> -->
+          <!-- 此处用v-for遍历表头，不是遍历数据 -->
+          <el-table-column v-for="(val, key) in tableLabel" :prop="key" :label="val"/>
+        </el-table>
+      </el-card>
+      
+    </el-col>
+    <el-col :span="16"><div class="grid-content bg-purple-light"></div></el-col>
+  </el-row>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      tableData: [
+          {
+            name: 'oppo',
+            todayBuy: 100,
+            monthBuy: 300,
+            totalBuy: 800
+          },
+          {
+            name: 'vivo',
+            todayBuy: 100,
+            monthBuy: 300,
+            totalBuy: 800
+          },
+          {
+            name: '苹果',
+            todayBuy: 100,
+            monthBuy: 300,
+            totalBuy: 800
+          },
+          {
+            name: '小米',
+            todayBuy: 100,
+            monthBuy: 300,
+            totalBuy: 800
+          },
+          {
+            name: '三星',
+            todayBuy: 100,
+            monthBuy: 300,
+            totalBuy: 800
+          },
+          {
+            name: '魅族',
+           todayBuy: 100,
+            monthBuy: 300,
+            totalBuy: 800
+          }
+        ],
+      tableLabel: {
+        name: '名称',
+        todayBuy: '今日购买',
+        monthBuy: '本月购买',
+        totalBuy: '总购买'
+      }
+    }
+  },
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.user {
+  display: flex;
+  align-items: center;
+  padding-bottom: 20px;
+  margin-bottom: 20px;
+  border-bottom: 1px solid #ccc;
+  img {
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+    margin-right: 40px;
+  }
+  .userinfo {
+    .name {
+      font-size: 32px;
+      margin-bottom: 10px;
+    }
+    .access {
+      color: #999;
+    }
+  }
+}
+.login-info {
+  p {
+    line-height: 28px;
+    font-size: 14px;
+    color: #999;
+    span {
+      color: #666;
+      margin-left: 60px;
+    }
+  }
+}
 </style>
